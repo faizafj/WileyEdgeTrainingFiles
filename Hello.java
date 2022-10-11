e76fd5f2972b1c9f33e0e5f18a66520ac323ab2f
Faiza Javed <faizafj@outlook.com>
2022-10-11 13:23:17 +0100

    Hello Java Files

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Faiza");
+        System.out.println("Address goes here!");
+        System.out.println("Phone number goes here");
+        System.out.println("Birimingham");
+    }
+}
